873bf6d5c80b2de37a40c880ebf99b0f9c1f23ff
DannyGlade <darshannariya12@gmail.com>
2024-07-29 18:21:24 -0400

    feat: Add search functionality to home screen

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,8 +1,52 @@
+import useBookApi from '@/hooks/useBookApi'
+import { selectBooks, selectBooksState } from '@/redux/booksSlice'
+import Ionicons from '@expo/vector-icons/Ionicons'
+import Feather from '@expo/vector-icons/Feather'
+import { useEffect, useState } from 'react'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import { YStack, Text, H1, ScrollView } from 'tamagui'
+import { useSelector } from 'react-redux'
+import {
+    YStack,
+    Text,
+    H1,
+    ScrollView,
+    Input,
+    XGroup,
+    Button,
+    YGroup,
+    Spinner,
+    Separator,
+} from 'tamagui'
+import BookTile from '@/components/BookTile'
 
 const Index = () => {
     const inset = useSafeAreaInsets()
+    const [loading, setLoading] = useState(false)
+    const [search, setSearch] = useState('')
+    const [page, setPage] = useState(1)
+    const { searchBooks, fetchBookImageURL } = useBookApi()
+    const bookState = useSelector(selectBooksState)
+
+    const handleSubmit = () => {
+        if (search === '') {
+            return
+        }
+        setLoading(true)
+        setPage(1)
+        searchBooks({ q: search, limit: 10, page: 1 }).then(() => {
+            setLoading(false)
+        })
+    }
+
+    useEffect(() => {
+        if (search !== '') {
+            setLoading(true)
+            searchBooks({ q: search, limit: 10, page }).then(() => {
+                setLoading(false)
+            })
+        }
+    }, [page])
+
     return (
         <ScrollView
             paddingTop={inset.top}
@@ -10,7 +54,101 @@ const Index = () => {
             backgroundColor={'$background025'}
         >
             <H1>📚 BookWorm</H1>
+
+            <YStack marginTop={16} gap={16} marginBottom={200}>
+                <XGroup>
+                    <Input
+                        flex={1}
+                        placeholder="Search"
+                        value={search}
+                        onChangeText={setSearch}
+                    />
+                    <Button
+                        disabled={loading}
+                        onPress={handleSubmit}
+                        icon={<Ionicons name="search" size={24} />}
+                    />
+                </XGroup>
+                <PaginationControls
+                    loading={loading}
+                    page={page}
+                    setPage={setPage}
+                    bookState={bookState}
+                />
+
+                <YGroup
+                    marginTop={16}
+                    separator={<Separator borderColor={'$background'} />}
+                >
+                    {loading ? (
+                        <>
+                            <Spinner size="large" />
+                            <Text>Loading...</Text>
+                        </>
+                    ) : (
+                        bookState.docs.map((book) => (
+                            <BookTile key={book.key} book={book} />
+                        ))
+                    )}
+                </YGroup>
+                <PaginationControls
+                    loading={loading}
+                    page={page}
+                    setPage={setPage}
+                    bookState={bookState}
+                />
+            </YStack>
         </ScrollView>
+    )
+}
+
+type PaginationControlsProps = {
+    loading: boolean
+    page: number
+    setPage: React.Dispatch<React.SetStateAction<number>>
+    bookState: ReturnType<typeof selectBooksState>
+}
+const PaginationControls = ({
+    loading,
+    page,
+    setPage,
+    bookState,
+}: PaginationControlsProps) => {
+    return (
+        <XGroup>
+            <Button
+                disabled={loading}
+                size={'$3'}
+                onPress={() => setPage(1)}
+                icon={<Feather name="chevrons-left" size={24} />}
+            />
+            <Button
+                disabled={loading}
+                size={'$3'}
+                onPress={() => setPage((prev) => prev - 1)}
+                icon={<Ionicons name="chevron-back" size={24} />}
+            />
+            <Input
+                size={'$3'}
+                textAlign="center"
+                flex={1}
+                value={`${page} / ${Math.ceil(bookState.numFound / 10)}`}
+            />
+            <Button
+                disabled={loading}
+                size={'$3'}
+                onPress={() => setPage((prev) => prev + 1)}
+                icon={<Ionicons name="chevron-forward" size={24} />}
+            />
+            <Button
+                disabled={loading}
+                size={'$3'}
+                onPress={() => {
+                    setPage(() => Math.ceil(bookState.numFound / 10))
+                }}
+                icon={<Feather name="chevrons-right" size={24} />}
+            />
+        </XGroup>
     )
 }
 
---
+++ components/BookTile.tsx
@@ -0,0 +1,46 @@
+import { Book } from '@/constants/Types'
+import useBookApi from '@/hooks/useBookApi'
+import { useRouter } from 'expo-router'
+import { H4, ListItem, YGroup, YStack, Text, Image } from 'tamagui'
+
+type BookTileProps = {
+    book: Book
+}
+const BookTile = ({ book }: BookTileProps) => {
+    const router = useRouter()
+    const { fetchBookImageURL } = useBookApi()
+
+    return (
+        <YGroup.Item key={book.key}>
+            <ListItem padding={0} icon={null} gap={16}>
+                <Image
+                    src={fetchBookImageURL({
+                        key: 'id',
+                        value: book.cover_i?.toString() ?? '',
+                        size: 'M',
+                    })}
+                    width={100}
+                    height={150}
+                />
+                <YStack width={'100%'} gap={4}>
+                    <H4 width={'60%'} numberOfLines={1}>
+                        {book.title}
+                    </H4>
+                    <YGroup>
+                        <Text fontWeight={'bold'}>Authors: </Text>
+                        <Text width={'60%'} numberOfLines={2}>
+                            {book.author_name.join(', ')}
+                        </Text>
+                    </YGroup>
+
+                    <YGroup>
+                        <Text fontWeight={'bold'}>Publish Year: </Text>
+                        <Text>{book.first_publish_year}</Text>
+                    </YGroup>
+                </YStack>
+            </ListItem>
+        </YGroup.Item>
+    )
+}
+
+export default BookTile
