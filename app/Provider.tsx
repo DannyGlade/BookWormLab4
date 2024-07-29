32998c00d515e4e1ec231eba1ec4fc72006e23cd
DannyGlade <darshannariya12@gmail.com>
2024-07-29 18:20:58 -0400

    chore: Update TamaguiProvider to include ReduxProvider

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,20 +1,22 @@
-import { useColorScheme } from "react-native";
-import { TamaguiProvider, type TamaguiProviderProps } from "tamagui";
-import { config } from "../tamagui.config";
+import { useColorScheme } from 'react-native'
+import { TamaguiProvider, type TamaguiProviderProps } from 'tamagui'
+import { config } from '../tamagui.config'
+import { Provider as ReduxProvider } from 'react-redux'
+import store from '@/redux/store'
 
 export function Provider({
-  children,
-  ...rest
-}: Omit<TamaguiProviderProps, "config">) {
-  const colorScheme = useColorScheme();
+    children,
+    ...rest
+}: Omit<TamaguiProviderProps, 'config'>) {
+    const colorScheme = useColorScheme()
 
-  return (
-    <TamaguiProvider
-      config={config}
-      defaultTheme={colorScheme === "dark" ? "dark" : "light"}
-      {...rest}
-    >
-      {children}
-    </TamaguiProvider>
-  );
+    return (
+        <TamaguiProvider
+            config={config}
+            defaultTheme={colorScheme === 'dark' ? 'dark' : 'light'}
+            {...rest}
+        >
+            <ReduxProvider store={store}>{children}</ReduxProvider>
+        </TamaguiProvider>
+    )
 }
--- redux/booksSlice.ts
+++ redux/booksSlice.ts
@@ -1,26 +1,37 @@
 import { createSelector, createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { Book } from '@/constants/Types'
+import { Book, searchApiResponseType } from '@/constants/Types'
 
 type BookPayload = PayloadAction<Book[]>
 
-const initialState: Book[] = []
+const initialState: searchApiResponseType = {
+    numFound: 0,
+    start: 0,
+    docs: [],
+    q: '',
+}
 
 const booksSlice = createSlice({
     name: 'books',
     initialState,
     reducers: {
-        setBooks: (state, action: BookPayload) => action.payload,
+        setBooks: (state, action: PayloadAction<searchApiResponseType>) => {
+            return action.payload
+        },
         clearBooks: () => initialState,
     },
 })
 
-export const selectBooks = (state: { books: Book[] }) => state.books
+export const selectBooksState = (state: { books: searchApiResponseType }) =>
+    state.books
+
+export const selectBooks = (state: { books: searchApiResponseType }) =>
+    state.books.docs
 
 export const selectBookByTitle = createSelector(
-    [selectBooks, (_: any, title: string) => title],
+    selectBooks,
+    (_: unknown, title: string) => title,
     (books, title) => books.find((book) => book.title === title)
 )
-
 export const { setBooks, clearBooks } = booksSlice.actions
 
 export default booksSlice.reducer
