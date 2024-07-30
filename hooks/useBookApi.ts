f0aaece66c1b606397ebc3500347f6db48497496
DannyGlade <darshannariya12@gmail.com>
2024-07-29 20:33:01 -0400

    chore: Update useBookApi to use OLID for author images

--- hooks/useBookApi.ts
+++ hooks/useBookApi.ts
@@ -9,7 +9,7 @@ import {
 
 const API_URL = 'https://openlibrary.org/search.json'
 const API_URL_BOOK_IMAGE = 'https://covers.openlibrary.org/b/id/'
-const API_URL_AUTHOR_IMAGE = 'https://covers.openlibrary.org/a/id/'
+const API_URL_AUTHOR_IMAGE = 'https://covers.openlibrary.org/a/olid/'
 
 export default function useBookApi() {
     const bookState = useSelector(selectBooks)
